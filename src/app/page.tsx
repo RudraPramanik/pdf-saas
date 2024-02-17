72310f8da97440d370ac945ce72b490f5f4f305b
Rudra <rudrahimangshoo2020@gmail.com>
2024-02-17 16:58:09 +0600

    drzzle-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
   const { userId } = auth()
   const isAuth = !!userId;
   return (
-    <main className="absolute bg-gradient-to-r from-rose-100 to-teal-100 inset-0 flex justify-center items-center">
-     <div className=" absolute top-1/2 left-1/2 translate-x-1/2 translate-y-1/2 " >
+    <main className=" w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 " >
       <div className="flex flex-col items-center text-center" >
         <div className="flex items-center" >
           <h1 className=" mr-3 text-5xl font-semibold  " >Chat with any pdf</h1>
@@ -19,7 +19,6 @@ export default function Home() {
         </div>
         <div className="flex mt-2">
           {isAuth && <Button>GO to chats</Button>}
-          <Button>Go to chat</Button>
         </div>
         <p className="max-w-xl mt-2 text-lg text-slate-600 " >
           Join millions of students , researchers and professionals to instantly questions and understand reaseach with AI
